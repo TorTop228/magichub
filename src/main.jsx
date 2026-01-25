11ac410f72b16d58cb1bc111e1478baacc3b70cc
TorTop228 <103282050+TorTop228@users.noreply.github.com>
2026-01-26 02:14:28 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import './styles/base-styles.css'
+
 import './styles/hub-styles.css'
 
 createRoot(document.getElementById('root')).render(
