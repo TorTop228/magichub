630a22c28d3f18f7015b7eed389aa126ad137393
TorTop228 <103282050+TorTop228@users.noreply.github.com>
2026-01-25 20:31:33 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-// Подключаем стили для landing и хаба
 import './styles/base-styles.css'
 import './styles/hub-styles.css'
 
